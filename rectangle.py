7f740ffacd37f22fbbcb10754b92db276863b7ab
Максим <73996350+F1eed@users.noreply.github.com>
2024-10-09 10:41:35 +0300

    Исправил комментарии

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,27 @@
-def area(a, b): #функция area(a, b) принимает два аргумента, возвращает их произведение
-        #area(a, b) -> int
-        return a * b  # area(5, 6) -> 30
+def area(a, b):
+        '''
+        Функция area(a, b) принимает два аргумента, возвращает их произведение
+            Параметры:
+                a(int)
+                b(int)
+                
+            Возвращаемое значение:
+                area(a, b) -> int
+                
+            area(5, 6) -> 30
+        '''
+        return a * b
 
-def perimeter(a, b): #функция perimeter(a, b) принимает два аргумента, возвращает их удвоенную сумму
-        #perimeter(a, b) -> int
-        return 2*(a + b) #perimeter(5, 6) -> 22
+def perimeter(a, b):
+        '''
+        Функция perimeter(a, b) принимает два аргумента, возвращает их удвоенную сумму
+            Параметры:
+                a(int)
+                b(int)
+
+            Возвращаемое значение:
+                perimeter(a, b) -> int
+                
+            perimeter(5, 6) -> 22
+        '''
+        return 2*(a + b)
--- triangle.py
+++ triangle.py
@@ -1,7 +1,30 @@
-def area(a, h): #функция area принимает два аргумента, возвращает полупроизведение аргументов
-    #area(a, h) -> int
-    return a * h / 2 #area(2,4) -> 4
+def area(a, h):
+    '''
+    Функция area принимает два аргумента, возвращает полупроизведение аргументов
 
-def perimeter(a, b, c): #функция perimeter принимает три аргумента, возвращает их сумму
-    #perimeter(a, b, c) -> int
-    return a + b + c #perimeter(3, 4, 5) -> 12
+        Параметры:
+            a(int)
+            h(int)
+            
+        Возвращаемое значение:
+            area(a, h) -> int
+            
+    area(2,4) -> 4
+    '''
+    return a * h / 2
+
+def perimeter(a, b, c):
+    '''
+    Функция perimeter принимает три аргумента, возвращает их сумму
+
+        Параметры:
+            a(int)
+            b(int)
+            c(int)
+            
+        Возвращаемое значение:
+            perimeter(a, b, c) -> int
+
+    perimeter(3, 4, 5) -> 12
+    '''
+    return a + b + c
